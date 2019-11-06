3d81287f5d342b06cd070632c95e1eae04533e8f
Eduardo Innarelli <eduinnarelli@gmail.com>
2019-11-06 11:55:08 -0300

    Initializing tabu search implementation

--- include/TabuSearchSVRP.h
+++ include/TabuSearchSVRP.h
@@ -1,3 +1,25 @@
 #include "SVRP.h"
 
-double penalizedExpectedLength(vector<vector<int>> routes, double penalty, int numVehicles);
+class TabuSearchSVRP {
+
+public:
+
+    Graph g;
+    double penalty;
+    int numVehicles, numSelected, numNearest;
+    int itCount, numInfeasibleNearby;
+    int currNoImprovement, maxNoImprovement;
+    vector<int> relativeDemand;
+    vector<vector<int>> routes, closestNeighbours;
+
+    // Etapas
+    TabuSearchSVRP(Graph g, int numVehicles);
+    void neighbourhoodSearch();
+    void incumbentUpdate();
+    void coefficientUpdate();
+    void intensification();
+
+    // Funções
+    double penalizedExpectedLength();
+
+};
---
+++ src/TabuSearchSVRP.cpp
@@ -0,0 +1,20 @@
+#include "TabuSearchSVRP.h"
+
+// Etapa 1: Inicialização
+TabuSearchSVRP::TabuSearchSVRP(Graph inst, int numVehicles) {
+
+    int h = min(inst.numberVertices - 1, 10);
+
+    // Computar os h vizinhos mais próximos de cada vértice
+    for (int i = 0; i < inst.numberVertices; i++) {
+
+        vector<int> closest(h);
+
+
+
+    }
+
+
+
+
+}
